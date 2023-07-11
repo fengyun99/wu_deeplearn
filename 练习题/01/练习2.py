9f340cf94183626f6dcff038a1f1d63b8908066b
fengyun99 <2234867425@qq.com>
2023-07-11 11:19:35 +0800

    生成200个激活码

--- 练习题/01/练习2.py
+++ 练习题/01/练习2.py
@@ -8,12 +8,23 @@
  做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），使用 Python 如何生成 200 个激活码（或者优惠券）
 '''
 import random
-up_letter = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-down_letter = 'qwertyuiopasdfghjklzxcvbnm'
-number = '0123456789'
-Str = up_letter + down_letter + number
-Promo_Code = []
-ALL_Promo_Code = []
-Length = 7
-for _ in range(Length):
-    Promo_Code.append(random.choice(Str))
+
+
+def gen_code():
+    up_letter = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    down_letter = 'qwertyuiopasdfghjklzxcvbnm'
+    number = '0123456789'
+    Str = up_letter + down_letter + number
+    Promo_Code = []
+    ALL_Promo_Code = []
+    Length = 7
+    for _ in range(Length):
+        Promo_Code.append(random.choice(Str))
+    return ''.join(Promo_Code)
+
+
+list_200 = []
+for _ in range(200):
+    list_200.append(gen_code())
+
+print(len(list_200))
